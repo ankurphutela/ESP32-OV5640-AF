1c492a5b7d774a2f803acd136d0b45ae8fe41996
ankurphutela <phutela.ankur@gmail.com>
2025-06-16 19:17:11 +0200

    Update ESP32_OV5640_AF.h

--- src/ESP32_OV5640_AF.h
+++ src/ESP32_OV5640_AF.h
@@ -1,63 +1,27 @@
-#ifndef ESP32_OV5640_AF_MANUAL_H
-#define ESP32_OV5640_AF_MANUAL_H
+/*
+  ESP32_OV5640_AF.h - Library for OV5640 Auto Focus (ESP32 Camera)
+  Created by Eric Nam, December 08, 2021.
+  Released into the public domain.
+*/
 
-#include "ESP32_OV5640_AF.h"
+#ifndef ESP32_OV5640_AF_h
+#define ESP32_OV5640_AF_h
 
-// VCM Control Registers
-#define OV5640_REG_VCM_CONTROL_1    0x3603
-#define OV5640_REG_VCM_CONTROL_0    0x3602
-#define OV5640_REG_VCM_CONTROL_2    0x3604
-#define OV5640_REG_VCM_CONTROL_3    0x3605
-#define OV5640_REG_VCM_CONTROL_4    0x3606
+#include <Arduino.h>
+#include "ESP32_OV5640_cfg.h"
+#include "esp_camera.h"
 
-// Focus positions
-#define OV5640_FOCUS_MIN            0      // Nearest focus
-#define OV5640_FOCUS_MAX            1023   // Farthest focus
-#define OV5640_FOCUS_DEFAULT        512    // Mid-range focus
-
-// Slew rate modes for focus movement speed
-#define OV5640_SLEW_DIRECT          0x00   // Direct jump mode
-#define OV5640_SLEW_SINGLE_50US     0x01   // 50µs steps
-#define OV5640_SLEW_SINGLE_100US    0x02   // 100µs steps
-#define OV5640_SLEW_SINGLE_200US    0x03   // 200µs steps
-#define OV5640_SLEW_SINGLE_400US    0x04   // 400µs steps
-#define OV5640_SLEW_SINGLE_800US    0x05   // 800µs steps
-#define OV5640_SLEW_SINGLE_1600US   0x06   // 1600µs steps
-#define OV5640_SLEW_SINGLE_3200US   0x07   // 3200µs steps
-
-class OV5640_Manual : public OV5640 {
-public:
-    OV5640_Manual();
-    
-    // Manual focus functions
-    bool initManualFocus();
-    bool setManualFocus(uint16_t position);
-    bool setManualFocus(uint16_t position, uint8_t slew_rate);
-    uint16_t getManualFocus();
-    bool releaseManualFocus();
-    bool enableVCMPower(bool enable);
-    
-    // Focus convenience functions
-    bool setFocusNear();
-    bool setFocusFar();
-    bool setFocusMid();
-    bool focusStep(int8_t steps);
-    
-    // Focus calibration helpers
-    bool calibrateNearLimit();
-    bool calibrateFarLimit();
-    uint16_t findOptimalFocus(uint8_t window_size = 5);
-    
+class OV5640 {
 private:
-    uint16_t current_focus_position;
-    uint16_t near_limit;
-    uint16_t far_limit;
-    bool vcm_powered;
-    
-    bool writeVCMRegister(uint16_t reg, uint8_t value);
-    uint8_t readVCMRegister(uint16_t reg);
-    bool waitForFocusComplete(uint32_t timeout_ms = 1000);
-    uint32_t calculateSharpness();
+  sensor_t* sensor;
+  bool isOV5640;
+
+public:
+  OV5640();
+  bool start(sensor_t* _sensor);
+  uint8_t focusInit();
+  uint8_t autoFocusMode();
+  uint8_t getFWStatus();
 };
 
 #endif
