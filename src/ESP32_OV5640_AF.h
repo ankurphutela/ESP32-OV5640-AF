75b3ee15a3cc6a4fcc9992aa62147f7585b63147
ankurphutela <phutela.ankur@gmail.com>
2025-06-16 19:05:35 +0200

    Update ESP32_OV5640_AF.h

--- src/ESP32_OV5640_AF.h
+++ src/ESP32_OV5640_AF.h
@@ -1,69 +1,63 @@
-/*
-  ESP32_OV5640_AF.h - Library for OV5640 Auto Focus (ESP32 Camera)
-  Modified version with Manual Focus support
-  Original Created by Eric Nam, December 23, 2021.
-  Released into the public domain.
-*/
+#ifndef ESP32_OV5640_AF_MANUAL_H
+#define ESP32_OV5640_AF_MANUAL_H
 
-#ifndef ESP32_OV5640_AF_h
-#define ESP32_OV5640_AF_h
+#include "ESP32_OV5640_AF.h"
 
-#include "Arduino.h"
-#include "sensor.h"
+// VCM Control Registers
+#define OV5640_REG_VCM_CONTROL_1    0x3603
+#define OV5640_REG_VCM_CONTROL_0    0x3602
+#define OV5640_REG_VCM_CONTROL_2    0x3604
+#define OV5640_REG_VCM_CONTROL_3    0x3605
+#define OV5640_REG_VCM_CONTROL_4    0x3606
 
-// Focus modes
-typedef enum {
-    AF_MODE_AUTO_CONTINUOUS = 0,
-    AF_MODE_AUTO_SINGLE = 1,
-    AF_MODE_MANUAL = 2,
-    AF_MODE_RELEASED = 3
-} focus_mode_t;
+// Focus positions
+#define OV5640_FOCUS_MIN            0      // Nearest focus
+#define OV5640_FOCUS_MAX            1023   // Farthest focus
+#define OV5640_FOCUS_DEFAULT        512    // Mid-range focus
 
-// Focus status
-typedef enum {
-    AF_STATUS_IDLE = 0,
-    AF_STATUS_FOCUSING = 1,
-    AF_STATUS_FOCUSED = 2,
-    AF_STATUS_NOT_FOCUSED = 3
-} focus_status_t;
+// Slew rate modes for focus movement speed
+#define OV5640_SLEW_DIRECT          0x00   // Direct jump mode
+#define OV5640_SLEW_SINGLE_50US     0x01   // 50µs steps
+#define OV5640_SLEW_SINGLE_100US    0x02   // 100µs steps
+#define OV5640_SLEW_SINGLE_200US    0x03   // 200µs steps
+#define OV5640_SLEW_SINGLE_400US    0x04   // 400µs steps
+#define OV5640_SLEW_SINGLE_800US    0x05   // 800µs steps
+#define OV5640_SLEW_SINGLE_1600US   0x06   // 1600µs steps
+#define OV5640_SLEW_SINGLE_3200US   0x07   // 3200µs steps
 
-class OV5640
-{
-  public:
-    OV5640();
+class OV5640_Manual : public OV5640 {
+public:
+    OV5640_Manual();
     
-    // Original functions
-    void start(sensor_t* _sensor);
-    byte sendCommand();
-    byte getFWStatus();
-    byte focusInit();
-    byte autoFocusMode();
-    byte infinityFocusMode();
+    // Manual focus functions
+    bool initManualFocus();
+    bool setManualFocus(uint16_t position);
+    bool setManualFocus(uint16_t position, uint8_t slew_rate);
+    uint16_t getManualFocus();
+    bool releaseManualFocus();
+    bool enableVCMPower(bool enable);
     
-    // New manual focus functions
-    byte setFocusMode(focus_mode_t mode);
-    byte setManualFocus(uint16_t focus_value);
-    byte triggerSingleAutoFocus();
-    byte releaseFocus();
-    uint16_t getCurrentFocusValue();
-    focus_status_t getFocusStatus();
+    // Focus convenience functions
+    bool setFocusNear();
+    bool setFocusFar();
+    bool setFocusMid();
+    bool focusStep(int8_t steps);
     
-    // Focus range functions
-    byte calibrateFocusRange();
-    uint16_t getMinFocusValue();
-    uint16_t getMaxFocusValue();
-    uint16_t mapDistanceToFocus(uint16_t distance_mm);
+    // Focus calibration helpers
+    bool calibrateNearLimit();
+    bool calibrateFarLimit();
+    uint16_t findOptimalFocus(uint8_t window_size = 5);
     
-  private:
-    sensor_t* sensor;
-    focus_mode_t current_mode;
-    uint16_t current_focus_value;
-    uint16_t min_focus_value;
-    uint16_t max_focus_value;
+private:
+    uint16_t current_focus_position;
+    uint16_t near_limit;
+    uint16_t far_limit;
+    bool vcm_powered;
     
-    // Internal functions
-    byte writeReg16(uint16_t reg, uint16_t value);
-    uint16_t readReg16(uint16_t reg);
+    bool writeVCMRegister(uint16_t reg, uint8_t value);
+    uint8_t readVCMRegister(uint16_t reg);
+    bool waitForFocusComplete(uint32_t timeout_ms = 1000);
+    uint32_t calculateSharpness();
 };
 
 #endif
